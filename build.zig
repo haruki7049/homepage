b4e38d1db7c552bcc5a3aaa2fac4801126207254
haruki7049 <tontonkirikiri@gmail.com>
2024-09-10 16:49:27 +0900

    hoge

--- build.zig
+++ build.zig
@@ -8,5 +8,7 @@ pub fn build(b: *std.Build) void {
         .layouts_dir_path = "layouts",
         .content_dir_path = "content",
         .assets_dir_path = "assets",
+
+        .static_assets = &.{ "fonts/UDEVGothicNF-Regular.ttf", "haruki7049.jpg", "favicon.ico", "use-nix_nixos.jpg" },
     });
 }
--- flake.nix
+++ flake.nix
@@ -5,23 +5,42 @@
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, treefmt-nix, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-      zig = pkgs.zig_0_13;
-    in {
-      devShells.default = pkgs.mkShell {
-        packages = [
-          # Zig
-          zig
-          pkgs.zls
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      treefmt-nix,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+        zig = pkgs.zig_0_13;
+      in
+      {
+        formatter = treefmtEval.config.build.wrapper;
 
-          # Nix
-          pkgs.nil
-        ];
-      };
-    });
+        checks = {
+          formatting = treefmtEval.config.build.check self;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            # Nix
+            pkgs.nil
+
+            # Ziglang
+            zig
+            pkgs.zls
+          ];
+
+          shellHook = ''
+            export PS1="\n[nix-shell\w]$ "
+          '';
+        };
+      }
+    );
 }
