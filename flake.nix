a911208952d04ecb117284e4b1d8ae115f3fe0be
haruki7049 <tontonkirikiri@gmail.com>
2025-03-23 16:17:09 +0900

    Update

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
+    flake-compat.url = "github:edolstra/flake-compat";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
@@ -16,7 +17,6 @@
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
-
       imports = [
         inputs.treefmt-nix.flakeModule
       ];
@@ -27,18 +27,13 @@
           treefmt = {
             projectRootFile = "flake.nix";
             programs.nixfmt.enable = true;
-            programs.zig.enable = true;
             programs.actionlint.enable = true;
           };
 
           devShells.default = pkgs.mkShell {
             packages = [
-              # Nix
               pkgs.nil
-
-              # Ziglang
-              pkgs.zig_0_13
-              pkgs.zls
+              pkgs.emacs-nox
             ];
 
             shellHook = ''
